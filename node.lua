10afaad501d0f51e24cbe98ab2fb15519816783b
majstrenko <142303133+majstrenko@users.noreply.github.com>
2023-11-27 22:46:07 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -9,7 +9,7 @@ local video_playing = false
 
 local function start_video(pin)
 
-    local videos = {
+local videos = {
     [16] = resource.load_video{file = "1.mp4"; looped = false; audio = true; paused = true},
     [17] = resource.load_video{file = "2.mp4"; looped = false; audio = true; paused = true},
     [18] = resource.load_video{file = "3.mp4"; looped = false; audio = true; paused = true},
