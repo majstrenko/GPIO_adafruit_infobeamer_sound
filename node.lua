a37c2cdf22e47838e602ce9233953e52335f1d59
majstrenko <142303133+majstrenko@users.noreply.github.com>
2023-11-27 22:12:21 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -67,8 +67,6 @@ function node.render()
         if video_state == "finished" then
             stop_video()
             gl.clear(1, 0, 0, 1) -- red, default state
-        else
-            current_video:draw(0, 0, WIDTH, HEIGHT)
         end
     else
         gl.clear(1, 0, 0, 1) -- red, default state
