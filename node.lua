39f1385b3e1f0e5a997d949fd44fafb85c0e07f8
majstrenko <142303133+majstrenko@users.noreply.github.com>
2023-11-27 12:35:41 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -3,27 +3,27 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 util.no_globals()
 
 local videos = {
-    [16] = resource.load_video{file = "1.mp4", looped = false, audio = true, paused = true},
-    [17] = resource.load_video{file = "2.mp4", looped = false, audio = true, paused = true},
-    [18] = resource.load_video{file = "3.mp4", looped = false, audio = true, paused = true},
-    [19] = resource.load_video{file = "4.mp4", looped = false, audio = true, paused = true},
+    [16] = {file = "1.mp4", video = nil},
+    [17] = {file = "2.mp4", video = nil},
+    [18] = {file = "3.mp4", video = nil},
+    [19] = {file = "4.mp4", video = nil},
 }
 
 local current_video = nil
 local video_playing = false
 
 local function start_video(pin)
-    if current_video then
-        current_video:stop()
-    end
+    stop_video() -- Stop any currently playing video
     current_video = videos[pin]
-    current_video:start()
+    current_video.video = resource.load_video{file = current_video.file, looped = false, audio = true, paused = true}
+    current_video.video:start()
     video_playing = true
 end
 
 local function stop_video()
-    if current_video then
-        current_video:stop()
+    if current_video and current_video.video then
+        current_video.video:dispose()
+        current_video.video = nil
     end
     current_video = nil
     video_playing = false
@@ -61,13 +61,13 @@ util.data_mapper{
 }
 
 function node.render()
-    if video_playing and current_video then
-        local video_state, w, h = current_video:state()
+    if video_playing and current_video and current_video.video then
+        local video_state, w, h = current_video.video:state()
         if video_state == "finished" then
             stop_video()
             gl.clear(1, 0, 0, 1) -- red, default state
         else
-            current_video:draw(0, 0, WIDTH, HEIGHT)
+            current_video.video:draw(0, 0, WIDTH, HEIGHT)
         end
     else
         gl.clear(1, 0, 0, 1) -- red, default state
