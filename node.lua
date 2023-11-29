1228eab73c9eda0181b9ffbd49c1a68a689743fa
majstrenko <142303133+majstrenko@users.noreply.github.com>
2023-11-29 17:13:02 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -55,11 +55,6 @@ util.data_mapper{
 function node.render()
     if video_playing and current_video then
         local video_state, w, h = current_video:state()
-        if video_state == "finished" then
-            stop_video()
-        else
-            current_video:draw(0, 0, WIDTH, HEIGHT)
-        end
     else
         gl.clear(1, 0, 0, 1) -- red, default state
     end
