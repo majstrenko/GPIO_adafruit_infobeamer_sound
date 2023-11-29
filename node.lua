7cfbbacf0b3e14faf1e70dcc94fca0439d0a56f5
majstrenko <142303133+majstrenko@users.noreply.github.com>
2023-11-29 21:33:08 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -32,26 +32,26 @@ end
 util.data_mapper{
     ["state/16"] = function(state)
         if state == '1' then
-            stop_video()
-            start_video(16)
+                        current_video:dispose()
+                        start_video(16)
         end
     end,
     ["state/17"] = function(state)
         if state == '1' then
-            stop_video()
-            start_video(17)
+                        current_video:dispose()
+                        start_video(17)
         end
     end,
     ["state/18"] = function(state)
         if state == '1' then
-            stop_video()
-            start_video(18)
+                        current_video:dispose()
+                        start_video(18)
         end
     end,
     ["state/19"] = function(state)
         if state == '1' then
-            stop_video()
-            start_video(19)
+                        current_video:dispose()
+                        start_video(19)
         end
     end,
 }
